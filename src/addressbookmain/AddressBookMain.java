470a0cd0dde99e0e0aa39f8b636eea04c3565bff
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-28 19:32:59 +0530

    Added : A service class to create contacts

--- src/addressbookmain/AddressBookMain.java
+++ src/addressbookmain/AddressBookMain.java
@@ -4,17 +4,13 @@ public class AddressBookMain
 {
 	public static void main(String[] args)
 	{
-		Contact contact1 = new Contact("jayesh", "mali", "Kharghar", "mumbai", "MH", "400211", "0987654321", "jay@gmail.com");
-		Contact contact2 = new Contact("jay", "mali", "Thane", "satara", "MH", "400211", "0987123345", "jay2@gmail.com");
-		Contact[] allContacts = new Contact[20];
-		allContacts[1] = contact1;			
-		allContacts[2] = contact2;			
-		for (int i = 1; i < allContacts.length; i++) {
-			if(allContacts[i] != null) 
-			{
-				System.out.println(allContacts[i]);
-			}
-		}
+		System.out.println("Welcome to Address Book");
+		AddressBookService contact = new AddressBookService();
+		contact.addContact();		
+		AddressBookService contact2 = new AddressBookService();
+		contact2.addContact();		
+		contact.diaplayContacts();
+		contact2.diaplayContacts();
 	}
 
 }
